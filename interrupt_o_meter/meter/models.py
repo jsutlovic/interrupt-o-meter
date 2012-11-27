bc65eb46d5169bc0f6b24b4279eb2879a0c7da74
Jero Sutlovic <jsutlovic@gmail.com>
2012-11-27 00:56:06 -0500

    Basic data models

--- interrupt_o_meter/meter/models.py
+++ interrupt_o_meter/meter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class DataPoint(models.Model):
+    name = models.CharField(max_length=255)
+    value = models.IntegerField()
+
+class DataPointDate(models.Model):
+    name = models.CharField(max_length=255)
+    value = models.DateTimeField()
+
+class DataDump(models.Model):
+    date = models.DateTimeField()
+    content = models.TextField()
